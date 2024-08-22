a60a39d287bfd2bd40cc8e0e4dc45694d76f51ab
RamvigneshA <ramvigneshnanban@gmail.com>
2024-08-22 23:03:03 +0530

    addes debounce function to hacked project

--- secret code/sample.js
+++ secret code/sample.js
@@ -2,13 +2,53 @@ const press = [];
 const scode = ['ArrowRight', 'ArrowUp', 'ArrowDown', 'Backspace']
 const heads = document.querySelectorAll('h1');
 
-window.addEventListener('keyup',(e)=>{
-	press.push(e.key);
-	const newarr = press.slice(-scode.length)
-	if(JSON.stringify(scode) === JSON.stringify(newarr)){
-		const header = [...heads];
-		header.map(heed => {
-			heed.innerText="hacked!"
-		})
-	}
-});
+
+
+function debounce(func, delay) {
+    let timeoutId;
+    
+    return function(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => func.apply(this, args), delay);
+    };
+}
+
+
+function secret(e){
+    press.push(e.key);
+    console.log(press);
+    const newarr = press.slice(-scode.length)
+    if(JSON.stringify(scode) === JSON.stringify(newarr)){
+        const header = [...heads];
+        header.map(heed => {
+            heed.innerText="hacked!"
+        })   
+
+    }
+}
+
+const debouncedKeyUp = debounce(secret, 100); // Debounce with 50ms delay
+
+window.addEventListener('keyup', debouncedKeyUp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
