2c55a8e872ebae5ebfdc2ba577e664cd034dbe18
RamvigneshA <ramvigneshnanban@gmail.com>
2024-08-25 23:37:15 +0530

    local storage done

--- localstorage and event delegation/script.js
+++ localstorage and event delegation/script.js
@@ -2,19 +2,25 @@ const addItemButton = document.getElementById('addItemButton');
 const ul = document.getElementById('ul');
 const textInput = document.querySelector('input');
 
-function addItem(event){
-    if(textInput.value){
-    event.preventDefault();
-   
-    const li= `<li>${textInput.value}</li>`;
-    // const li = document.createElement('li');
-    // li.innerText = textInput.value;
-    ul.innerHTML += li; 
-    textInput.value = '';
-    }else{
-    event.preventDefault();
+const items = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
+function addItem(event){
+    event.preventDefault();
+    const item ={
+        text: textInput.value
     }
+    
+    items.push(item);
+    itemList(items,ul);
+    localStorage.setItem('items',JSON.stringify(items));
+    textInput.value="";
+
 }
 
-addItemButton.addEventListener('click',addItem);
+
+function itemList(sample=[],ulelement){
+    ulelement.innerHTML= sample.map(item=> `<li>${item.text}</li>`).join('');
+
+}
+addItemButton.addEventListener('submit',addItem);
+addItemButton.addEventListener('reload', itemList(items,ul));
