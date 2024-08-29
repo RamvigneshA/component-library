b0f3d0b8bcc549701816aabf70cd3f506bfc3987
RamvigneshA <ramvigneshnanban@gmail.com>
2024-08-29 19:13:44 +0530

    added stop and start functions

--- Text to voice convertor/script.js
+++ Text to voice convertor/script.js
@@ -15,4 +15,22 @@ function poptVoices() {
 
 speechSynthesis.addEventListener('voiceschanged', poptVoices);
 
+stopButton.addEventListener('click', () => {
+    speechSynthesis.cancel();
+});
+speakButton.addEventListener('click', () => {
+    msg.text = document.querySelector('#text').value;
+    speechSynthesis.speak(msg);
+});
+
+options.forEach(option => {
+    option.addEventListener('change', e => {
+        
+        msg[e.target.name] = e.target.value;
+    });
+});
+voicesSelect.addEventListener('change', e => {
+    console.log(e.target.value);
+    msg.voice = voices.find(voice => voice.name ===e.target.value);
+});
  
